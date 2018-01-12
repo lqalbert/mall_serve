4e2d0fb272447c834ca9ce570585bbccc24cf7f5
yrg <huj@riign.com>
2018-01-12 16:16:27 +0800

    20180112合并后

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-//         $this->call(UsersTableSeeder::class);
-        $this->call(UserTableSeeder2::class);
+         $this->call(UsersTableSeeder::class);
+//        $this->call(UserTableSeeder2::class);
     }
 }
