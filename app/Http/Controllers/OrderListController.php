a80aa65e72391d9551931494d3e420ca857c0d57
mcdyqnc <hyfdyx@qq.com>
2018-01-15 15:09:43 +0800

    fix orderlist

---
+++ app/Http/Controllers/OrderListController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Orderlist;
+use Illuminate\Http\Request;
+use App\Repositories\OrderlistRepository;
+use App\Services\Orderlist\OrderlistService;
+use App\Repositories\Criteria\Orderlist\Time;
+
+class OrderlistController extends Controller
+{
+    //
+    
+    private $repository = null;
+    public function  __construct(OrderlistRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $business = $request->query('business', 'default');
+        $result = [];
+        switch ($business){
+            case 'Orderlist':
+                $service = app('App\Services\Orderlist\OrderlistService');
+                $result = $service->get();
+                break;
+            case 'select':
+                $service = app('App\Services\Orderlist\OrderlistService');
+                $result = $service->get();
+                break;
+            default:
+                $service = app('App\Services\Orderlist\OrderlistService');
+                $result = $service->get();
+        }
+        return $result;
+    }
+}
