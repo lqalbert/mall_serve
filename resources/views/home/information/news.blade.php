464192dc688de723a4fdd9a6e6c5571518327542
mcdyqnc <hyfdyx@qq.com>
2018-01-26 19:24:14 +0800

    pro

--- resources/views/home/information/news.blade.php
+++ resources/views/home/information/news.blade.php
@@ -39,38 +39,39 @@
                 <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 rightContent">
                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 rightTit">为你推荐</div>
-                        <div class="col-lg-12 col-md-12 col-sm-6 col-xs-12 rightCon">
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 left"></div>
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 right">
-                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 tit">一大堆标题水电费</div>
-                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 dat">2017-10-12</div>
-                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 scan">浏览量：1558</div>
-                            </div>
-                        </div>
-                        <div class="col-lg-12 col-md-12 col-sm-6 col-xs-12 rightCon">
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 left"></div>
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 right">
-                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 tit">一大堆标题水电费</div>
-                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 dat">2017-10-12</div>
-                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 scan">浏览量：1558</div>
-                            </div>
-                        </div>
-                        <div class="col-lg-12 col-md-12 col-sm-6 col-xs-12 rightCon">
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 left"></div>
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 right">
-                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 tit">一大堆标题水电费</div>
-                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 dat">2017-10-12</div>
-                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 scan">浏览量：1558</div>
-                            </div>
-                        </div>
-                        <div class="col-lg-12 col-md-12 col-sm-6 col-xs-12 rightCon">
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 left"></div>
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 right">
-                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 tit">一大堆标题水电费</div>
-                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 dat">2017-10-12</div>
-                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 scan">浏览量：1558</div>
-                            </div>
-                        </div>
+                         @foreach ($articles as $article)
+	                    	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 leftCos">
+	                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 pic">
+	                            	<img alt="" style="height: 300px;width: 100%;" src="{{ $article->image }}">
+	                            </div>
+	                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 tit">{{ $article->title }}</div>
+	                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 con">{{ $article->description }}</div>
+	                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 dat">{{ $article->created_at }}   
+	                            </div>
+	                        </div>
+	                        <div class="col-lg-12 col-md-12 col-sm-6 col-xs-12 rightCon">
+	                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 left">
+	                            	<img alt="{{ $article->title }}"  src="{{ $article->image }}">
+	                            </div>
+	                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 right">
+	                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 tit">{{ $article->title }}</div>
+	                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 dat">{{ $article->created_at }}</div>
+<!-- 	                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 scan">浏览量：1558</div> -->
+	                            </div>
+	                        </div>
+					    @endforeach
+<!--                         <div class="col-lg-12 col-md-12 col-sm-6 col-xs-12 rightCon"> -->
+<!--                             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 left"></div> -->
+<!--                             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 right"> -->
+<!--                                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 tit">一大堆标题水电费</div> -->
+<!--                                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 dat">2017-10-12</div> -->
+<!--                                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 scan">浏览量：1558</div> -->
+<!--                             </div> -->
+<!--                         </div> -->
+                        
+                        
+                        
+                        
                     </div>
                 </div>
             </div>
