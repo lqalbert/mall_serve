481bdd29553167ecfc56c5115955f1bea85ecf12
yrg <huj@riign.com>
2018-01-29 14:01:09 +0800

    20180129定金和员工信息修改

--- app/Models/User.php
+++ app/Models/User.php
@@ -85,7 +85,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'created_at',
         'updated_at',
         'deleted_at'
     ];
--- database/migrations/2018_01_21_182525_create_deposits_table.php
+++ database/migrations/2018_01_21_182525_create_deposits_table.php
@@ -16,12 +16,15 @@ class CreateDepositsTable extends Migration
         Schema::create('deposit_basic', function (Blueprint $table) {
         	//为了方便设置nullable
             $table->increments('id');
-            $table->unsignedInteger('department_id')->nullable();
-            $table->unsignedInteger('group_id')->nullable();
-            $table->unsignedInteger('user_id')->nullable();
-            $table->string('department_name', 30)->nullable();
-            $table->string('group_name', 30)->nullable();
-            $table->string('realname', 30)->nullable();
+            $table->unsignedInteger('department_id')->nullable()->comment('部门ID');
+            $table->unsignedInteger('group_id')->nullable()->comment('小组ID');
+            $table->unsignedInteger('user_id')->nullable()->comment('员工ID');
+            $table->unsignedInteger('charge_type')->comment('充值方式')->nullable();
+            $table->date('charge_time')->nullable()->comment('充值时间');
+            $table->string('department_name', 30)->nullable()->comment('部门名称');
+            $table->string('group_name', 30)->nullable()->comment('小组名称');
+            $table->string('realname', 30)->nullable()->comment('员工姓名');
+            $table->string('remark')->nullable()->comment('备注');
             $table->decimal('money', 8, 2)->default('0.00');
             $table->unsignedInteger('creator_id')->nullable();
             $table->string('creator',30)->nullable();
