768f34428b30c6596b3c97578b6f6b77d2f752b8
mcdyqnc <hyfdyx@qq.com>
2018-04-04 09:21:19 +0800

    1.0.9

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -12,15 +12,15 @@
 
     <!-- Styles -->
 <!--     <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
-    <link href="/admin/static1.0.8/css/app.67ea6678302334be2ab947aa2fca5ba4.css" rel="stylesheet">
+    <link href="/admin/static1.0.9/css/app.0b2cb92d8ae15035bdde23bd2171988d.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
     </div>
 
     <!-- Scripts -->
-	<script type="text/javascript" src="/admin/static1.0.8/js/manifest.047db0a0e1b31c49cf35.js"></script>
-    <script type="text/javascript" src="/admin/static1.0.8/js/vendor.d611ecf87c51152c9c1f.js"></script>
-    <script type="text/javascript" src="/admin/static1.0.8/js/app.8e35caf0c3ccf30c8d40.js"></script>
+	<script type="text/javascript" src="/admin/static1.0.9/js/manifest.0688501bb08564446837.js"></script>
+    <script type="text/javascript" src="/admin/static1.0.9/js/vendor.d611ecf87c51152c9c1f.js"></script>
+    <script type="text/javascript" src="/admin/static1.0.9/js/app.bf32818d8719557aa34a.js"></script>
 </body>
 </html>
