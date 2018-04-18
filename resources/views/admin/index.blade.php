122cbc2031c71b3cb9b7e30f683e9f87b2a75ca5
mcdyqnc <hyfdyx@qq.com>
2018-04-18 17:22:50 +0800

    version

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -12,15 +12,16 @@
 
     <!-- Styles -->
 <!--     <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
-	<link href="/admin/static1.0.14/css/app.fc7e8a44ece2d87e03de373afe76715f.css" rel="stylesheet">
+	<!--[if IE]><script src="https://cdn.jsdelivr.net/npm/promise-polyfill@7/dist/polyfill.min.js"></script><![endif]-->
+    <link href="/admin/static1.0.15/css/app.104810e46a47570f20044c3cf8fabb8b.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
     </div>
 
     <!-- Scripts -->
-	<script type="text/javascript" src="/admin/static1.0.14/js/manifest.b78f9d071ffd39e3ec20.js"></script>
-    <script type="text/javascript" src="/admin/static1.0.14/js/vendor.18c8cadfa9001265e9df.js"></script>
-    <script type="text/javascript" src="/admin/static1.0.14/js/app.f28297558969ef8a57d7.js"></script>
+	<script type="text/javascript" src="/admin/static1.0.15/js/manifest.aee1f4bb52ffdf93ad02.js"></script>
+    <script type="text/javascript" src="/admin/static1.0.15/js/vendor.18c8cadfa9001265e9df.js"></script>
+    <script type="text/javascript" src="/admin/static1.0.15/js/app.f28297558969ef8a57d7.js"></script>
 </body>
 </html>
