16d77fe64716d9dd2661c269e4b17e840ca8d588
mcdyqnc <hyfdyx@qq.com>
2018-04-20 15:21:20 +0800

    wefwef: i Please enter the commit message for your changes. Lines starting

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,15 +13,15 @@
     <!-- Styles -->
 <!--     <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
 	<!--[if IE]><script src="https://cdn.jsdelivr.net/npm/promise-polyfill@7/dist/polyfill.min.js"></script><![endif]-->
-    <link href="/admin/static1.0.19/css/app.008dd333f8b581dcafa152b683d4f034.css" rel="stylesheet">
+    <link href="/admin/static1.0.20/css/app.c6402aa82792f9e584cfd72c291c4951.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
     </div>
 
     <!-- Scripts -->
-	<script type="text/javascript" src="/admin/static1.0.19/js/manifest.3abb6d4c12ab09409065.js"></script>
-    <script type="text/javascript" src="/admin/static1.0.19/js/vendor.18c8cadfa9001265e9df.js"></script>
-    <script type="text/javascript" src="/admin/static1.0.19/js/app.a0a274f1087ffa78cf10.js"></script>
+	<script type="text/javascript" src="/admin/static1.0.20/js/manifest.9c8668579131ae2483fe.js"></script>
+    <script type="text/javascript" src="/admin/static1.0.20/js/vendor.18c8cadfa9001265e9df.js"></script>
+    <script type="text/javascript" src="/admin/static1.0.20/js/app.a0a274f1087ffa78cf10.js"></script>
 </body>
 </html>
