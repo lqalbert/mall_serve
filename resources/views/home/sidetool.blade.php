0de640b63c7d10c308c1c09d363a4d56b3d9e269
mcdyqnc <hyfdyx@qq.com>
2018-05-04 15:50:01 +0800

    fhy

---
+++ resources/views/home/sidetool.blade.php
@@ -0,0 +1,22 @@
+<div id="scrollBox">
+    <div class="customerService sbox">
+        <img src="/images/home/index/customerservice.png" alt=""/>
+        <div class="content">客服服务</div>
+    </div>
+    <div class="options sbox">
+        <img src="/images/home/index/option.png" alt=""/>
+        <div class="content">意见反馈</div>
+    </div>
+    <div class="qrcode sbox">
+        <img src="/images/home/index/qrcode.png" alt=""/>
+        <div class="content">
+            <div id="qrcode">
+            	<img src="/images/fcode.png"/>
+            </div>
+        </div>
+    </div>
+    <div id="scrollTop" class="scrollTop sbox">
+        <img src="/images/home/index/top.png" alt=""/>
+        <div class="content">返回顶部</div>
+    </div>
+</div>
