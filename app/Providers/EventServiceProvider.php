f51dcdcd6dae98bba910078a0dce3156906ed737
mcdyqnc <hyfdyx@qq.com>
2018-05-21 09:41:06 +0800

    fix

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -14,9 +14,9 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\Event' => [
-            'App\Listeners\EventListener',
-        ],
+//         'App\Events\Event' => [
+//             'App\Listeners\EventListener',
+//         ],
         
         'App\Events\AddEmployee' => [
             'App\Listeners\SyncEmployeeRoleListener',
@@ -46,6 +46,7 @@ class EventServiceProvider extends ServiceProvider
         'App\Events\OrderCreating' => [
             'App\Listeners\OrderCreatingListener'
         ],
+        
         'App\Events\OrderCancel' => [
             //库存
             'App\Listeners\OrderCancelListener',
@@ -66,7 +67,7 @@ class EventServiceProvider extends ServiceProvider
             'App\Listeners\OrderPassCheckedListener',
             'App\Listeners\DepositDecrementListener', //扣保证金
             'App\Listeners\InventorySetAssignListener', //通知库存 进行对应的更新
-            'App\Listeners\CreateAssignListener',//生成配货单 生成发货锁定
+            //'App\Listeners\CreateAssignListener',//生成配货单 生成发货锁定
         ],
         
         'App\Events\OrderUnPass' => [],
@@ -79,30 +80,30 @@ class EventServiceProvider extends ServiceProvider
         //生成发货记录 不用了 配货单自动就有了
         //库存表 更新对应的字段
         // @todo 修改订单状态 为 已发货
-        'App\Events\DeliverGoods' => [
-            'App\Listeners\InventoryAssignedListener'
-        ],
+//         'App\Events\DeliverGoods' => [
+//             'App\Listeners\InventoryAssignedListener'
+//         ],
            
         //签名
-        'App\Events\Signatured' => [
-            'App\Listeners\InventorySignaturedListener'
-        ],
+//         'App\Events\Signatured' => [
+//             'App\Listeners\InventorySignaturedListener'
+//         ],
         
         //售后　@todo 没有生成对应的配货单
-        'App\Events\AddAfterSale' => [
-            'App\Listeners\ChangeOrderAfterSaleListener', //改售后状态
-            'App\Listeners\ChangeExchangeAfterSaleListener' //如果是换货则需要 换货锁定 
+//         'App\Events\AddAfterSale' => [
+//             'App\Listeners\ChangeOrderAfterSaleListener', //改售后状态
+//             'App\Listeners\ChangeExchangeAfterSaleListener' //如果是换货则需要 换货锁定 
                 
-        ],
+//         ],
         
         'App\Events\ContactConflict' => [
             'App\Listeners\ContactconflictListener'
         ],
         
         //仓库换货一个商品
-        'App\Events\ProductExchange' => [
-            'App\Listeners\ChangeExchangeAfterSaleUnLockListener' //解锁
-        ]
+//         'App\Events\ProductExchange' => [
+//             'App\Listeners\ChangeExchangeAfterSaleUnLockListener' //解锁
+//         ]
         
         
     ];
