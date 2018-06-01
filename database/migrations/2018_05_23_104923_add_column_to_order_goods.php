9461e0e9cdb408f0e93cf9b44ebbf6fb89b9261e
mcdyqnc <hyfdyx@qq.com>
2018-06-01 14:08:54 +0800

    fxi

--- database/migrations/2018_05_23_104923_add_column_to_order_goods.php
+++ database/migrations/2018_05_23_104923_add_column_to_order_goods.php
@@ -17,7 +17,7 @@ class AddColumnToOrderGoods extends Migration
             $table->string('width',6)->nullable()->after('exchange_status')->comment('包装规格宽mm');
             $table->string('height',6)->nullable()->after('width')->comment('包装规格高mm');
             $table->string('len',6)->nullable()->after('height')->comment('包装规格长mm');
-            $table->string('barcode')->nullable()->after('length')->comment('条码');
+            $table->string('barcode')->nullable()->after('len')->comment('条码');
             $table->string('weight',20)->nullable()->after('barcode')->comment('重量(g)');
             $table->string('bubble_bag',20)->nullable()->after('weight')->comment('气泡垫(g)');
         });
@@ -31,7 +31,7 @@ class AddColumnToOrderGoods extends Migration
     public function down()
     {
         Schema::table('order_goods', function (Blueprint $table) {
-            $table->dropColumn(['width','height','length','barcode','weight','bubble_bag']);
+            $table->dropColumn(['width','height','len','barcode','weight','bubble_bag']);
         });
     }
 }
