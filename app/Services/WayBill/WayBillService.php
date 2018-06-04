6db1a9ac864d5a812ef683d28ac5930ece68ca10
mcdyqnc <hyfdyx@qq.com>
2018-06-04 09:43:44 +0800

    面单获取未完成

---
+++ app/Services/WayBill/WayBillService.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Services\WayBill;
+
+use App\models\OrderBasic;
+
+class WayBillService 
+{
+    public function __construct($param) {
+        ;
+    }
+    
+    public function getANew(OrderBasic $order)
+    {
+        //连接LINK PAC
+        
+        return '';
+    }
+    
+    
+}
