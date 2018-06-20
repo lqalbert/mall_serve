438430274bf664b650a962de974fbd8ab7680d18
mcdyqnc <hyfdyx@qq.com>
2018-06-20 11:51:25 +0800

    iaawe

--- resources/views/print/index.blade.php
+++ resources/views/print/index.blade.php
@@ -24,8 +24,8 @@
         }
         h1{
             font-size: 30pt;
-            height: 15mm;
-            line-height:15mm;
+            height: 13mm;
+            line-height:13mm;
 /*             line-height:10mm; */
             margin: 5mm auto 5mm;
         }
