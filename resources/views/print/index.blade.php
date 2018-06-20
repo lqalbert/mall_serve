177e1e9f7de24ccb22bf03b7d3a5e6f658d2360a
mcdyqnc <hyfdyx@qq.com>
2018-06-20 11:48:56 +0800

    position

--- resources/views/print/index.blade.php
+++ resources/views/print/index.blade.php
@@ -118,7 +118,7 @@
 </head>
 <body>
 	<div class="container " style="position: relative;">
-		<img src="/images/print_logo.fw.png" style="vertical-align:middle; position:absolute; top:-50%;" width="130"/>
+		<img src="/images/print_logo.fw.png" style="vertical-align:middle; position:absolute; 0px" width="130"/>
         <h1 class="text-center">宝贝清单</h1>
 	</div>
     <div class="container">
