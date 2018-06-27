05872557cbd1825b7a96b7ca67fae0e5d05d3112
mcdyqnc <hyfdyx@qq.com>
2018-06-27 10:26:43 +0800

    try IE polyfill and fix some bugs

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,7 +13,7 @@
     <!-- Styles -->
 <!--     <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
 	<!-- ><script src="https://cdn.jsdelivr.net/npm/promise-polyfill@7/dist/polyfill.min.js"></script>< -->
-    <link href="/admin/static1.1.18/css/app.b4e4a0b67ed55dbd545f27cee6b56916.css" rel="stylesheet">
+    <link href="/admin/static1.1.20/css/app.fbefac6ec2cee4721aee099cac89a45b.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -28,8 +28,8 @@
     } 
    </script>
     <!-- Scripts -->
-	<script type="text/javascript" src="/admin/static1.1.18/js/manifest.90e5539c8674afac81ca.js"></script>
-    <script type="text/javascript" src="/admin/static1.1.18/js/vendor.97cddee02d1dbc0846b4.js"></script>
-    <script type="text/javascript" src="/admin/static1.1.18/js/app.fd579a7f16ec19c2f503.js"></script>
+	<script type="text/javascript" src="/admin/static1.1.20/js/manifest.4b7105dd79765d62695d.js"></script>
+    <script type="text/javascript" src="/admin/static1.1.20/js/vendor.3d45132bb20a333cc660.js"></script>
+    <script type="text/javascript" src="/admin/static1.1.20/js/app.d88cd72b9a06635888a6.js"></script>
 </body>
 </html>
