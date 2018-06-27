f09ca9b1d2b9b5357d25d4ea92db4ed88e1ed338
mcdyqnc <hyfdyx@qq.com>
2018-06-27 16:28:59 +0800

    ef

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,24 +13,24 @@
     <!-- Styles -->
 <!--     <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
 	<!-- ><script src="https://cdn.jsdelivr.net/npm/promise-polyfill@7/dist/polyfill.min.js"></script>< -->
-    <link href="/admin/static1.1.21/css/app.36625d30c2ff1d94a9e6304e93b7fb22.css" rel="stylesheet">
+    <link href="/admin/static1.1.22/css/app.dbd3d697d48789de327320d281e0fa50.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
     </div>
-	<script>
-	//给IE10的 上面的 条件注释在IE10以上的浏览器不起作用了y
-    if(isIE = navigator.userAgent.indexOf("MSIE")!=-1) { 
-        var h = document.getElementsByTagName('head');
-        var scr = document.createElement('script');
-        scr.setAttribute('src', 'https://cdn.jsdelivr.net/npm/promise-polyfill@7/dist/polyfill.min.js');
-        h[0].appendChild(scr);
-    } 
-   </script>
+<!-- 	<script> 
+// 	//给IE10的 上面的 条件注释在IE10以上的浏览器不起作用了y
+//     if(isIE = navigator.userAgent.indexOf("MSIE")!=-1) { 
+//         var h = document.getElementsByTagName('head');
+//         var scr = document.createElement('script');
+//         scr.setAttribute('src', 'https://cdn.jsdelivr.net/npm/promise-polyfill@7/dist/polyfill.min.js');
+//         h[0].appendChild(scr);
+//     } 
+   </script>-->
     <!-- Scripts -->
-	<script type="text/javascript" src="/admin/static1.1.21/js/manifest.d84c5b97ea82087dbde0.js"></script>
-    <script type="text/javascript" src="/admin/static1.1.21/js/vendor.0c122c06ebcaba843bc4.js"></script>
-    <script type="text/javascript" src="/admin/static1.1.21/js/babel-polyfill.9e41b68c2bb89c9472cf.js"></script>
-    <script type="text/javascript" src="/admin/static1.1.21/js/app.fd579a7f16ec19c2f503.js"></script>
+	<script type="text/javascript" src="/admin/static1.1.22/js/manifest.04a91a749a30c1565e2c.js"></script>
+    <script type="text/javascript" src="/admin/static1.1.22/js/vendor.0c122c06ebcaba843bc4.js"></script>
+    <script type="text/javascript" src="/admin/static1.1.22/js/babel-polyfill.9e41b68c2bb89c9472cf.js"></script>
+    <script type="text/javascript" src="/admin/static1.1.22/js/app.fd579a7f16ec19c2f503.js"></script>
 </body>
 </html>
