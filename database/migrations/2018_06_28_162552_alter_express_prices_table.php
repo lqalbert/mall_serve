5e5ab561fb5b593c73f8eb669820578c92556007
yrg <huj@riign.com>
2018-06-28 17:14:44 +0800

    20180628修改快递价格表字段信息

---
+++ database/migrations/2018_06_28_162552_alter_express_prices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterExpressPricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table('express_prices', function (Blueprint $table) {
+                $table->unsignedInteger('first_weight')->default(0)->comment('首重')->change();
+                $table->decimal('first_price')->default(0)->comment('首价')->change();
+                $table->unsignedInteger('first_weight')->default(0)->comment('续重')->change();
+                $table->decimal('continued_price')->default(0)->comment('续价')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+//        Schema::table('express_prices', function (Blueprint $table) {
+//            $table->dropColumn(['first_weight','first_price','continued_weight','continued_price']);
+//        });
+    }
+}
