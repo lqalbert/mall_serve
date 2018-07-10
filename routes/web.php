25111944b82bde1e58141d5206c0d63b7b4fc32f
mcdyqnc <hyfdyx@qq.com>
2018-07-10 17:48:59 +0800

    没改什么

--- routes/web.php
+++ routes/web.php
@@ -168,7 +168,6 @@ Route::group($adminGroup, function(){
 	//电子面单
 	Route::get('/getOne/{assign_id}/{express_id}', 'WayBillController@getOne')
 	->where(['assign_id'=>'[0-9]+','express_id' => '[0-9]+', 'order_id' => '[0-9]+']);
-	
 	Route::put('/order-after-sale-check/{id}', 'AfterSaleController@checkStatus');
 	Route::put('/order-after-sale-sure/{id}', 'AfterSaleController@sureStatus');
 	Route::get('/cus-all-info/{id}', 'AfterSaleController@getCusAllInfo');
@@ -179,7 +178,6 @@ Route::group($adminGroup, function(){
 	Route::resource('/actualdeliverygoods',  'ActualDeliveryGoodsController');
 	
 	Route::get('/sale-quan', 'SaleQuanController@index');
-	
     Route::get('/salesperformance',  'SalesPerformanceController@index');
     Route::get('/salesperformance-selectorder',  'SalesPerformanceController@selectOrder');
 
