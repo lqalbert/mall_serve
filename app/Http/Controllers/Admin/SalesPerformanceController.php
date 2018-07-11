aad789e1e6bc6d0048aec01fc6e6189a5bddf852
yrg <huj@riign.com>
2018-07-11 14:01:41 +0800

    20180711

--- app/Http/Controllers/Admin/SalesPerformanceController.php
+++ app/Http/Controllers/Admin/SalesPerformanceController.php
@@ -59,7 +59,8 @@ class SalesPerformanceController extends Controller
         $start = $request->input('start')." 00:00:00"; //'2018-01-01 00:00:00';
         $end = $request->input('end')." 23:59:59"; //'2018-02-02 23:59:59';
         $groupBy = $request->input('type');
-        
+        $pageSize = $request->input('pageSize', 20);
+        $offset = ($request->input('page',1) -1) * $pageSize;
         $where = [];
         $where[]=['db.created_at','>=', $start];
         $where[]=['db.created_at','<=', $end];
@@ -74,11 +75,12 @@ class SalesPerformanceController extends Controller
                 'db.created_at as traded_at',
                 'db.order_sn',
                 'customer_basic.name as cus_name',
-                'customer_contact.phone as cus_phone'
+                'order_address.phone as cus_phone'
             )
             ->leftJoin('order_after','db.id','=','order_after.order_id')
             ->leftJoin('customer_basic','customer_basic.id','=','db.cus_id')
-            ->leftJoin('customer_contact','customer_contact.cus_id','=','db.cus_id')
+//            ->leftJoin('customer_contact','customer_contact.cus_id','=','db.cus_id')
+            ->leftJoin('order_address','order_address.order_id','=','db.id')
             ->where($where)->get();
         return [
             'items'=>$result,
