88756d48cff319f65ff1c3e620001a63499bd60a
mcdyqnc <hyfdyx@qq.com>
2018-07-31 17:34:11 +0800

    f

--- app/Http/Controllers/Home/ProductController.php
+++ app/Http/Controllers/Home/ProductController.php
@@ -24,7 +24,7 @@ class ProductController extends CommonController
         //还是有问题 的 如果 type 有多个非一级分类就会出错
         $goodsTypeName=GoodsType::has('cate')->get();
         $goodsTypeName->load('cate');
-        
+        logger("[dd]", $goodsTypeName->toArray());
         foreach ($goodsTypeName as $k=>$v){
             $subNav[$v->cate->id] = [
                 'url'=>route('product/index', ['cate_id'=>$v->cate->id]),
