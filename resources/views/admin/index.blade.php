f7476529cbf1ee4da705bd0056b0e35255b95335
mcdyqnc <hyfdyx@qq.com>
2018-08-07 19:27:53 +0800

    faf

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,17 +13,17 @@
     <!-- Styles -->
 <!--     <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
 	<!-- ><script src="https://cdn.jsdelivr.net/npm/promise-polyfill@7/dist/polyfill.min.js"></script>< -->
-    <link href="/admin/static1.1.77/css/app.38a9d324c54f7bca0bb4fb38c13e7d6d.css" rel="stylesheet">
+    <link href="/admin/static1.1.78/css/app.245a5f4fa4086288bf474dbc1899c537.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
        
     </div>
     <!-- Scripts -->
-    <script type="text/javascript" src="/admin/static1.1.77/js/manifest.890ea57af633d47bccee.js"></script>
-    <script type="text/javascript" src="/admin/static1.1.77/js/vendor.a2f5e9af0088cdf77e28.js"></script>
-    <script type="text/javascript" src="/admin/static1.1.77/js/babel-polyfill.c973c152d3451c47c753.js"></script>
-    <script type="text/javascript" src="/admin/static1.1.77/js/app.30fbb826c85ddfb7b970.js"></script>
+    <script type="text/javascript" src="/admin/static1.1.78/js/manifest.10499b43a304e67a4103.js"></script>
+    <script type="text/javascript" src="/admin/static1.1.78/js/vendor.a2f5e9af0088cdf77e28.js"></script>
+    <script type="text/javascript" src="/admin/static1.1.78/js/babel-polyfill.c973c152d3451c47c753.js"></script>
+    <script type="text/javascript" src="/admin/static1.1.78/js/app.30fbb826c85ddfb7b970.js"></script>
     
     <script src="https://s19.cnzz.com/z_stat.php?id=1274133839&web_id=1274133839" language="JavaScript"></script>
 </body>
