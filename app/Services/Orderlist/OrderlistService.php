4e29d40dfcf7e93ed3880e3d4aa12b336fe8461b
mcdyqnc <hyfdyx@qq.com>
2018-08-09 17:19:11 +0800

    itest

--- app/Services/Orderlist/OrderlistService.php
+++ app/Services/Orderlist/OrderlistService.php
@@ -118,8 +118,7 @@ class OrderlistService
             $this->repository->pushCriteria(new FieldEqual('deal_id', $this->request->deal_id));
         }
         if ($this->request->has('department_id')) {
-            // $where[]=['department_id','=',$this->request->department_id];
-            $this->repository->pushCriteria(new FieldEqual('department_id', $this->request->department_id));
+            $this->repository->pushCriteria(new FieldEqual('department_id', $this->request->input('department_id')));
         }
         if ($this->request->has('group_id')) {
             // $where[]=['group_id','=',$this->request->group_id];
