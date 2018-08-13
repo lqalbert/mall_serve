952bb95a713f35fe4441f5a0c509aac3d80d9329
mcdyqnc <hyfdyx@qq.com>
2018-08-13 11:18:51 +0800

    fa

--- resources/views/home/nav.blade.php
+++ resources/views/home/nav.blade.php
@@ -11,7 +11,7 @@
                     </div>
             		<div class="row">
             			<div class="col-lg-2 col-md-2 col-sm-3 col-xs-12 logoImg">
-                            <img src="images/home/index/logo.png" alt="">
+                            <img src="/images/home/index/logo.png" alt="">
                         </div>
             			<div class="col-lg-7 col-md-7 col-sm-6 col-xs-12">
                     <!-- Brand and toggle get grouped for better mobile display -->
