a231b0460f868149d076aa0a9087ff5ebe41ae2e
mcdyqnc <hyfdyx@qq.com>
2018-08-23 13:47:26 +0800

    test

--- app/Services/WayBill/MsgType/TmsWayBillGet2.php
+++ app/Services/WayBill/MsgType/TmsWayBillGet2.php
@@ -46,7 +46,7 @@ class TmsWayBillGet2 extends TmsWayBillGet
                 'userId'=>$userId, //使用者ID
             ];
         }
-        
+        logger('[get2]', $result);
         return $result;
     }
 }
