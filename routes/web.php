f6a98e02c2dead443b5707652f7ac1f3335b9ee4
mcdyqnc <hyfdyx@qq.com>
2018-08-25 16:18:24 +0800

    fa

--- routes/web.php
+++ routes/web.php
@@ -111,6 +111,7 @@ Route::group($adminGroup, function(){
 	Route::put('/order-assign-repeat/{id}', 'AssignController@repeatOrder');
 	Route::put('/order-assign-stop/{id}',  'AssignController@stopOrder');
 	Route::post('/assign-waybill-print/{id}', 'AssignController@waybillPrint');
+	Route::post('/assign-waybill-prints', 'AssignController@waybillPrints');
 	Route::post('/assign-goods-print/{id}', 'AssignController@goodsPrint');
 	Route::get('/assign-goods-prints', 'AssignController@goodsPrint2');
 	Route::put('/assign-checkgoods/{id}', 'AssignController@checkGoods');
