fea1767e37835975e8780a05c78bab353884a1c6
yerui <2425423423@qq.com>
2018-08-27 11:04:53 +0800

    底部导航增加公众号二维码，产品列表去掉边框个

--- resources/views/home/navBottom.blade.php
+++ resources/views/home/navBottom.blade.php
@@ -1,7 +1,7 @@
 <div class=" navBottom">
     <div class="container">
     	<div class="row navBot">
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 box">
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 box">
                     <div class="title">关于普拉她</div>
                     <div class="">
                         <a href="{{URL('brand/index')}}">
@@ -10,7 +10,7 @@
                     </div>
                     <div class=""><a href="{{URL('information/company')}}">公司信息</a></div>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 box">
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 box">
                     <div class="title">美丽帮助</div>
                     <div class="">
                         {{----}}
@@ -26,7 +26,7 @@
                     </div>
                     {{--<div class="">0371-888888</div>--}}
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 box">
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 box">
                     <div class="title">客户服务</div>
                     <div class="">
                         
@@ -36,12 +36,17 @@
                     </div>
                     <div class=""><a href="{{ action('Home\QuestionController@index') }}?{{ http_build_query(['exchange'=>1])}}">退换货问题</a></div>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 box">
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 box">
                     <div class="title">联系我们</div>
                     <div class="">微商城</div>
                     <div class="">公众号</div>
                 </div>
-           
+            <div id="weiCode">
+                <img src="/images/code.jpg" alt="">
+                <div class="minCode">
+                    <img src="/images/code.jpg" alt="">
+                </div>
+            </div>
         </div>
         
     </div>
