1e91a3233a380f42130a2371c465d91f7ad83db3
mcdyqnc <hyfdyx@qq.com>
2018-09-03 10:40:29 +0800

    display question

--- config/menug.php
+++ config/menug.php
@@ -4,7 +4,7 @@ return [
         [
             'text'=>'工作台管理',
             'icon'=>null,
-            'subIndex'=>['Workbench','SysNotice','Contacts','Website','SlideManage']
+            'subIndex'=>['Workbench','SysNotice','Contacts','Website','SlideManage','QuestionnaireManagement']
         ],
 
 
