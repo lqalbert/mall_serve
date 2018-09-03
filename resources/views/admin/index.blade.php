b3796424982825719234ff952f41cfa53aa75ecd
mcdyqnc <hyfdyx@qq.com>
2018-09-03 19:35:27 +0800

    version 1.2.14bbg

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,17 +13,17 @@
     <!-- Styles -->
 <!--     <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
 	<!-- ><script src="https://cdn.jsdelivr.net/npm/promise-polyfill@7/dist/polyfill.min.js"></script>< -->
-   <link href="/admin/static1.2.14bbf/css/app.5119955c55dbfc1f26a6a92f22ae3634.css" rel="stylesheet">
+   <link href="/admin/static1.2.14bbg/css/app.5730dcbd8d7e49b7aa3c8c00a1aee0eb.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
        
     </div>
     <!-- Scripts -->
-    <script type="text/javascript" src="/admin/static1.2.14bbf/js/manifest.49d50c0d267e9cbae12a.js"></script>
-    <script type="text/javascript" src="/admin/static1.2.14bbf/js/vendor.65d05dc61052b653a7c1.js"></script>
-    <script type="text/javascript" src="/admin/static1.2.14bbf/js/app.87d341995fc207cd1483.js"></script>
-    <script type="text/javascript" src="/admin/static1.2.14bbf/js/babel-polyfill.d68a272d98d7d77b51aa.js"></script>
+    <script type="text/javascript" src="/admin/static1.2.14bbg/js/manifest.a2af7799ea4274dda4a4.js"></script>
+    <script type="text/javascript" src="/admin/static1.2.14bbg/js/vendor.65d05dc61052b653a7c1.js"></script>
+    <script type="text/javascript" src="/admin/static1.2.14bbg/js/app.c5c1cc5db065b4ea3afd.js"></script>
+    <script type="text/javascript" src="/admin/static1.2.14bbg/js/babel-polyfill.d68a272d98d7d77b51aa.js"></script>
     
     <script src="https://s19.cnzz.com/z_stat.php?id=1274133839&web_id=1274133839" language="JavaScript"></script>
 </body>
