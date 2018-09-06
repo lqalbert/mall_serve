2e58b5cb9eea27fbc8da808a1fb5366c99a2a248
mcdyqnc <hyfdyx@qq.com>
2018-09-06 14:44:24 +0800

    修改了打包问题的第一个版本

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,17 +13,26 @@
     <!-- Styles -->
 <!--     <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
 	<!-- ><script src="https://cdn.jsdelivr.net/npm/promise-polyfill@7/dist/polyfill.min.js"></script>< -->
-    <link href="/admin/static1.2.15test/css/app.d9d37d5ef21f065a4ccfd0d953d75822.css" rel="stylesheet">
+    <link href="/admin/static1.2.16/css/app.ad6a0f2941bc45399fe6f2d74b5e029f.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
        
     </div>
     <!-- Scripts -->
-    <script type="text/javascript" src="/admin/static1.2.15test/js/manifest.c086dda26a351a3c2e68.js"></script>
-    <script type="text/javascript" src="/admin/static1.2.15test/js/vendor.68e868ef3ba290f4db9a.js"></script>
-    <script type="text/javascript" src="/admin/static1.2.15test/js/app.3130b2b718c64959baf2.js"></script>
-    <script type="text/javascript" src="/admin/static1.2.15test/js/babel-polyfill.d5221a2aaf662c3a6b9a.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.js"></script>
+    <script src="https://unpkg.com/element-ui@1.4/lib/index.js"></script>
+    <script type="text/javascript" src="/admin/static1.2.16/js/manifest.8a574e0a24ef8821b03f.js"></script>
+    <script type="text/javascript" src="/admin/static1.2.16/js/vendor.cf3e80b3b03fb3a37bfa.js"></script>
+    <script type="text/javascript" src="/admin/static1.2.16/js/babel-polyfill.d5221a2aaf662c3a6b9a.js"></script>
+    <script type="text/javascript" src="/admin/static1.2.16/js/app.898fdeda3161ed71a16b.js"></script>
+    
+    
+<!--     <script type="text/javascript" src="/admin/static1.2.15testa/js/manifest.a1c89676b0f61a324fc2.js"></script> -->
+<!--     <script type="text/javascript" src="/admin/static1.2.15testa/js/vendor.39944016b7833f0a0315.js"></script> -->
+<!--     <script type="text/javascript" src="/admin/static1.2.15testa/js/babel-polyfill.641c079dfe69390af014.js"></script> -->
+<!--     <script type="text/javascript" src="/admin/static1.2.15testa/js/app.1f3fe6695d652d1159ae.js"></script> -->
+<!--     <script type="text/javascript" src="/admin/static1.2.15testa/js/vendor1.cee2a3230c9e1d3a286c.js"></script> -->
     
     <script src="https://s19.cnzz.com/z_stat.php?id=1274133839&web_id=1274133839" language="JavaScript"></script>
 </body>
