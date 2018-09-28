2f6a1851f7735e076ab028f2ca0e568b4db799a2
mcdyqnc <hyfdyx@qq.com>
2018-09-28 16:05:44 +0800

    version 1.2.26

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,7 +13,7 @@
     <!-- Styles -->
 <!--     <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
 	<!-- ><script src="https://cdn.jsdelivr.net/npm/promise-polyfill@7/dist/polyfill.min.js"></script>< -->
-    <link href="/admin/static1.2.25a/css/app.b85a28511da4e242a21aebc3a0a93301.css" rel="stylesheet">
+    <link href="/admin/static1.2.26/css/app.8a79074a2e5c15db5da1a216d66e6b17.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -22,10 +22,10 @@
 <!--     <script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.js"></script> -->
 <!--     <script src="https://unpkg.com/element-ui@1.4/lib/index.js"></script> -->
     <!-- Scripts -->
-   	<script type="text/javascript" src="/admin/static1.2.25a/js/manifest.041cea3a9d8bb1b20e80.js"></script>
-    <script type="text/javascript" src="/admin/static1.2.25a/js/vendor.68e868ef3ba290f4db9a.js"></script>
-    <script type="text/javascript" src="/admin/static1.2.25a/js/babel-polyfill.d5221a2aaf662c3a6b9a.js"></script>
-    <script type="text/javascript" src="/admin/static1.2.25a/js/app.a216f96a8d6a226dbac0.js"></script>
+   	<script type="text/javascript" src="/admin/static1.2.26/js/manifest.d48b78bea20cc2e3ed0c.js"></script>
+    <script type="text/javascript" src="/admin/static1.2.26/js/vendor.68e868ef3ba290f4db9a.js"></script>
+    <script type="text/javascript" src="/admin/static1.2.26/js/babel-polyfill.d5221a2aaf662c3a6b9a.js"></script>
+    <script type="text/javascript" src="/admin/static1.2.26/js/app.a216f96a8d6a226dbac0.js"></script>
     
     <script src="https://s19.cnzz.com/z_stat.php?id=1274133839&web_id=1274133839" language="JavaScript"></script>
 </body>
