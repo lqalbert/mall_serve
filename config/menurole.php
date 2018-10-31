e2b96c4c95b63b75b6165d8444d7c0acf2acc0e7
mcdyqnc <hyfdyx@qq.com>
2018-10-31 18:27:47 +0800

    fa

--- config/menurole.php
+++ config/menurole.php
@@ -12,7 +12,7 @@ return [
     'sale-manager' => [
         'GoodsDetails',
         'Group',
-        'Employee',
+//         'Employee',
         'Customer',
         'OrderList',
         'BuyOrders',
