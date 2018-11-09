d84933b08afdfca66a913b97abb3124ff5bf7452
mcdyqnc <hyfdyx@qq.com>
2018-11-09 17:27:30 +0800

    f

--- app/Services/DepositOperation/SaleAlgorithm.php
+++ app/Services/DepositOperation/SaleAlgorithm.php
@@ -39,7 +39,7 @@ class SaleAlgorithm  extends AbstractAlgorithm
      */
     public function appendReturn($amount)
     {
-        return rund($this->calculate($amount, $this->setModel->getZn() * ( 1 - $this->setModel->getYz() )) - $this->setModel->getC()*$amount,2);
+        return round($this->calculate($amount, $this->setModel->getZn() * ( 1 - $this->setModel->getYz() )) - $this->setModel->getC()*$amount,2);
     }
     
     /**
