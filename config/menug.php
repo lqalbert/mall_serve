89c8c7312fd7f5e4ec9acab13556bf60401149d6
mcdyqnc <hyfdyx@qq.com>
2018-11-10 09:55:59 +0800

    fa

--- config/menug.php
+++ config/menug.php
@@ -26,7 +26,7 @@ return [
 		[
 				'text' => '客户订单',
 				'icon'=>null,
-		    'subIndex'=>['Customer','OrderList', 'Refund', 'OrderType', 'SampleApplication','JdOrderBasic'] // 'Refund' 'JdOrderBasic'
+		    'subIndex'=>['Customer','OrderList', 'Refund', 'OrderType', 'SampleApplication'] // 'Refund' 'JdOrderBasic'
 		],
 		[
 				'text' => '库存管理',
